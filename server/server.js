6e6d885955c7fdf442e93278ce60a1271b3d39b5
Jijo Joy <jijojoy@gmail.com>
2018-10-22 09:03:37 +0530

    socket initial checkin

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const path = require('path')
-const app = express()
+const socketIO = require('socket.io')
+const http = require('http')
 
+const app = express()
 const port = process.env.PORT || 3000
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(express.static(path.join(__dirname,'../public')))
 
-app.listen(port,()=>{
+io.on('connection', (socket) => {
+    console.log('new user connected')
+
+    socket.on('disconnect', () =>{
+        console.log('Client Disconnected')
+    })
+})
+
+server.listen(port,()=>{
     console.log(`server is up and running on ${port}`)
 })
